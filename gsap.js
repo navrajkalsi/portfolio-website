d278f1431c105a1236eccbc3e3eca117f2b4ac24
Navraj Kalsi <navrajkalsi@icloud.com>
2025-09-23 19:05:27 -0400

    css snap

--- gsap.js
+++ gsap.js
@@ -1,4 +1,3 @@
-// let split = SplitText.create(".info-text"
 gsap.from(".reveal-y", { opacity: 0, y: 100, duration: 0.5 });
 gsap.to(".scale-up", { opacity: 1, scale: 1, duration: 1, delay: 0.5 });
 gsap.from(".info", { opacity: 0, y: 100, duration: 0.5, delay: 1.5 });
